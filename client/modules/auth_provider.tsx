7848a73f0d6974ae167340b7f3445757c2b505c3
TarunCore <112856864+TarunCore@users.noreply.github.com>
2024-07-03 12:16:30 +0530

    files added

---
+++ client/modules/auth_provider.tsx
@@ -0,0 +1,63 @@
+import { useState, createContext, useEffect } from 'react'
+import { useRouter } from 'next/router'
+
+export type UserInfo = {
+  username: string
+  id: string
+  token?: string
+}
+
+export const AuthContext = createContext<{
+  authenticated: boolean
+  setAuthenticated: (auth: boolean) => void
+  user: UserInfo
+  setUser: (user: UserInfo) => void
+}>({
+  authenticated: false,
+  setAuthenticated: () => {},
+  user: { username: '', id: '', token: '' },
+  setUser: () => {},
+})
+
+const AuthContextProvider = ({ children }: { children: React.ReactNode }) => {
+  const [authenticated, setAuthenticated] = useState(false)
+  const [user, setUser] = useState<UserInfo>({ username: '', id: '', token: '' })
+
+  const router = useRouter()
+
+  useEffect(() => {
+    const userInfo = localStorage.getItem('user_info')
+
+    if (!userInfo) {
+      if (window.location.pathname != '/signup') {
+        router.push('/login')
+        return
+      }
+    } else {
+      const user: UserInfo = JSON.parse(userInfo)
+      if (user) {
+        setUser({
+          username: user.username,
+          id: user.id,
+          token: user.token
+        })
+      }
+      setAuthenticated(true)
+    }
+  }, [authenticated])
+
+  return (
+    <AuthContext.Provider
+      value={{
+        authenticated: authenticated,
+        setAuthenticated: setAuthenticated,
+        user: user,
+        setUser: setUser,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  )
+}
+
+export default AuthContextProvider
